07b033890561cbe6454c57f46f67fa21774c0a2d
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2019-01-02 13:20:17 +0100

    Add DI to console app

---
+++ MusicInside.Batch.Importer/Infrastructure/BatchDependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MusicInside.DataAccessLayer.Context;
+using NLog.Extensions.Logging;
+using System.IO;
+
+namespace MusicInside.Batch.Importer.Infrastructure
+{
+    public class BatchDependencyInjection
+    {
+        public static ServiceProvider BuildContainer()
+        {
+            // Retrieve base path
+            string BasePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+
+            // Set up fon configuration sources
+            var confBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true);
+
+            IConfiguration conf = confBuilder.Build();
+
+            return new ServiceCollection()
+                .AddLogging(builder => {
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                    builder.AddNLog(new NLogProviderOptions
+                    {
+                        CaptureMessageTemplates = true,
+                        CaptureMessageProperties = true
+                    });
+                })
+                .AddDbContext<MusicInsideDbContext>(options => options.UseSqlServer(conf.GetConnectionString("MusicInsideDatabase")))
+                .Configure<MusicFilesOptions>(conf.GetSection("MusicFiles"))
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ MusicInside.Batch.Importer/Infrastructure/MusicFilesOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicInside.Batch.Importer.Infrastructure
+{
+    public class MusicFilesOptions
+    {
+        public string RootDirectory { get; set; }
+        public string CoverSubFolder { get; set; }
+        public string RegexSubFolder { get; set; }
+        public string[] AvailableExtensions { get; set; }
+    }
+}
